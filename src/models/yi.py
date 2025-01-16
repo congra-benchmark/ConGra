713637626c39f37290c979e0c85599bcd7276ba3
congra-benchmark <An0nmou52424@gmail.com>
2025-01-16 21:33:25 +0800

    initialize repo

---
+++ src/models/yi.py
@@ -0,0 +1,18 @@
+""" 
+Yi Module
+"""
+from langchain_openai import ChatOpenAI
+
+
+class Yi(ChatOpenAI):
+    """
+    A wrapper for the ChatOpenAI class (Yi uses an OpenAI-like API) that
+    provides default configuration and could be extended with additional methods
+    if needed.
+
+    Args:
+        llm_config (dict): Configuration parameters for the language model.
+    """
+
+    def __init__(self, llm_config: dict):
+        super().__init__(**llm_config)
